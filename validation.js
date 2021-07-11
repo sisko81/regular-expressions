6f34ffe8b1cd127cc99c3955d1b2e0770004ecb8
Sisko Ben <siskoben@g.com>
2021-07-12 08:27:37 +0900

    Add validation.js

---
+++ validation.js
@@ -0,0 +1,25 @@
+const inputs = document.querySelectorAll("input");
+
+const patterns = {
+telephone:/^\d{11}$/,
+username:/^[a-z\d]{5,12}$/i,
+Password:/^[\w\@-]{8,20}$/,
+slug:/^[a-z\d-]{8,20}$/,
+email:/^([a-z\d\._-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+};
+
+function validate(field,regex){
+    if (regex.test(field.value)){
+        field.className = "valid";
+    }else {
+        field.className = "invalid";
+    }
+    
+}
+
+inputs.forEach((input)=> {
+  input.addEventListener("keyup", (e) =>{
+      //console.log(e.target.attributes.name.value);
+      validate(e.target, patterns[e.target.attributes.name.value])
+  });
+});
